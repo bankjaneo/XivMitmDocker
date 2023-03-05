cadd02bcbfdec0a6fec41fd9b70f5ac0589a69dd
bankjaneo <p@wd.is>
2023-03-05 19:15:35 +0700

    Get server IP from opcodes json

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# Clean up old iptables rules
-/app/cleanup.sh
-
 SCRIPT_URL="https://raw.githubusercontent.com/Soreepeong/XivMitmLatencyMitigator/main/mitigate.py"
 DEVICE_NAME=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
 
-# Check iptables-legacy
+# Check Internet connection function
+check_internet () {
+    while ! curl --connect-timeout 5 -sfL $SCRIPT_URL > /dev/null; do
+        echo "No Internet connection. Retry in 5 seconds."
+        sleep 5
+    done
+}
+
+# Create server IP list.
+check_internet
+definition_urls=`curl -s https://api.github.com/repos/Soreepeong/XivAlexander/contents/StaticData/OpcodeDefinition | jq -r '.[] | select(.name|test(".json$")) | .download_url '`
+> server-list.txt
+for url in $definition_urls; do
+    curl -s $url | jq -r '.Server_IpRange' | sed -e 's/, /\n/g' >> server-list.txt
+done
+if [ ! -s server-list.txt ]; then
+    cp backup-server-list.txt server-list.txt
+fi
+
+# Clean up old iptables rules.
+/app/cleanup.sh
+
+# Check if iptables-legacy is set.
 if [ "$LEGACY" = "true" ]; then
     update-alternatives --set iptables /usr/sbin/iptables-legacy
 fi
@@ -29,7 +48,8 @@ else
     if [ "$VPN" = "true" ]; then
         for var in "${!VPN_INTERFACE_@}"; do
             VPN_SUBNET=$(ip addr show ${!var} | grep "inet\b" | awk '{print $2}')
-            
+
+            # Create iptables fules for VPN routing.
             while IFS="" read -r SERVER_IP || [ -n "$SERVER_IP" ]; do
                 iptables -t nat -A POSTROUTING -s $VPN_SUBNET -d $SERVER_IP -o $DEVICE_NAME -j MASQUERADE
             done < server-list.txt
@@ -41,13 +61,9 @@ fi
 
 # Running mitigate.py
 if [ -z ${MITIGATOR+x} ] || [ "$MITIGATOR" = "true" ]; then
-    # Check for internet connection before downloading script.
-    while ! curl --connect-timeout 5 -sfL $SCRIPT_URL > /dev/null; do
-        echo "Cannot download script. Retry in 5 seconds."
-        sleep 5
-    done
-
     curl $SCRIPT_URL -o mitigate.py
+
+    # Convert iptables to iptables-legacy if set.
     if [ "$LEGACY" = "true" ]; then
         sed -i "s/iptables -t/iptables-legacy -t/" mitigate.py
     fi
